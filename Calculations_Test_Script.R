2adffc8792a830b1b99e3d7a4f085c8bee79c4ae
Justin Mendoza <49258832+Justintime8@users.noreply.github.com>
2021-08-01 15:45:23 -0500

    Add files via upload

---
+++ Calculations_Test_Script.R
@@ -0,0 +1,7 @@
+# first calculation 
+quarter_1_sales <- 3456.54
+quarter_2_sales <- 4381.90
+midyear_sales <- quarter_1_sales + quarter_2_sales
+midyear_sales
+yearend_sales < - midyear_sales * 2
+yearend_sales
