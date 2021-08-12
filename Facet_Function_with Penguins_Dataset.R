bfede985e404895cb5a00f30fce28f075f9be36e
Justin Mendoza <49258832+Justintime8@users.noreply.github.com>
2021-08-12 12:09:26 -0500

    Add files via upload

--- Facet_Function_with Penguins_Dataset.R
+++ Facet_Function_with Penguins_Dataset.R
@@ -11,3 +11,9 @@ library(tidyverse)
 library(palmerpenguins)
 
 ggplot(data=penguins)+ geom_point(mapping=aes(x=flipper_length_mm,y=body_mass_g,color=species))+ facet_wrap(~species)
+
+ggplot(data=penguins)+ geom_point(mapping=aes(x=flipper_length_mm,y=body_mass_g,color=species))+ facet_grid(sex~species)
+
+ggplot(data=penguins)+ geom_point(mapping=aes(x=flipper_length_mm,y=body_mass_g,color=species))+ facet_grid(~species)
+
+ggplot(data=penguins)+ geom_point(mapping=aes(x=flipper_length_mm,y=body_mass_g,color=species))+ facet_grid(~sex)
